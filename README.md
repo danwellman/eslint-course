d6e1071ed40b99f3307723ee71d526a594f81ef1
Dan Wellman <danwellman@hotmail.com>
2017-05-26 21:14:49 +0100

    Added plugin

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # eslint-course
 The example code for the Modern JavaScript Linting With ESLint course at Tuts+
+
+Copy the require-iife.js file to `node_modules/eslint/lib/rules`
---
+++ require-iife.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+
+module.exports = {
+    meta: {
+        docs: {
+            description: 'Require IIFE around code in all files',
+            category: 'Stylistic issues',
+            recommended: false
+        }
+    },
+    create(context) {
+
+        const ERROR = {
+            MESSAGE: 'All files should be wrapped in an IIFE'
+        }
+
+        function report(node) {
+            context.report({
+                node,
+                message: ERROR.MESSAGE
+            });
+        }
+
+        return {
+            onCodePathStart: function (codePath, node) {
+                if (node.tokens && node.tokens[0].value !== '(' && node.tokens[1].value !== 'function') {
+                    report(node);
+                }
+            }
+        }
+    }
+};
